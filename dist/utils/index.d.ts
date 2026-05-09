5d57408c4384b7e89529d8022ba3b1778653f782
roman.lo <roman@dida.com>
2026-05-09 09:15:36 +0800

    chore: update package.json for husky and add release dry run script
    - Changed the prepare script to use husky directly.
    - Added a new release:dry script for dry runs of the release process.
    - Specified pnpm and node versions under the volta field.

--- dist/utils/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './interact-helper';
-export * from './css-units';
--- es/components/DraggableResizableWrapper.vue2.mjs
+++
@@ -1,5 +0,0 @@
-import f from "./DraggableResizableWrapper.vue.mjs";
-export {
-  f as default
-};
-//# sourceMappingURL=DraggableResizableWrapper.vue2.mjs.map
